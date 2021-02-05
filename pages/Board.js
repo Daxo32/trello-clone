11dfb79919c9f39795c8d6e02fced03544dff87a
Davide Canci <davide.canci98@icloud.com>
2021-02-05 21:50:44 +0100

    initial commit

---
+++ pages/Board.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+export default function Board(props) {
+    useEffect(() => {
+        const data = {}
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
