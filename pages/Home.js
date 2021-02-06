11db7ea37a41aa55f9859331e067f9933a57543a
Davide Canci <davide.canci98@icloud.com>
2021-02-06 17:34:07 +0100

    First version

--- pages/Home.js
+++ pages/Home.js
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react"
-import { Row, Container, Col } from "shards-react";
+import { Row, Container, Col } from "shards-react"
 import Board from '../components/Board'
+import Navbar from '../components/NavbarComp'
+
 
 function Home() {
-    const [tasks_data, setTasks_data] = useState([
-        {
-            text: 'gary',
-            id: 1
-        },
-        {
-            text: 'jhon',
-            id: 2
-        },
-        {
-            text: 'red',
-            id: 3
-        }
-    ])
+
+
+    const [tasks_data, setTasks_data] = useState([{ text: "Default" }])
     return (
         <Container fluid>
-            <Row>
-                <Col>
-                    <Board title="first" tasks={tasks_data} />
-                </Col>
-
-            </Row>
+            <Navbar />
+            <Container>
+                <br /><br /><br />
+                <Row>
+                    <Col lg="4" sm="12">
+                        <Board key="1" title="Da fare" tasks={[]} />
+                    </Col>
+                    <Col lg="4" sm="12">
+                        <Board key="2" title="Fatto" tasks={[]} />
+                    </Col>
+                    <Col lg="4" sm="12">
+                        <Board key="3" title="Urgente" tasks={[]} />
+                    </Col>
+                </Row>
+            </Container>
         </Container>
+
     )
 }
 
