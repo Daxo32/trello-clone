442c3e77c639c614fd0dc4b02842342852570408
Davide Canci <davide.canci98@icloud.com>
2021-02-15 15:04:50 +0100

    ref start

--- pages/Home.js
+++ pages/Home.js
@@ -2,19 +2,20 @@ import { useEffect, useState } from "react"
 import { Row, Container, Col, Button } from "shards-react"
 import Board from '../components/Board'
 import Navbar from '../components/NavbarComp'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import styles from '../styles/Home.module.css'
 
 
 function Home() {
 
-    const [boards, setBoards] = useState(
-        []
-    )
+    const [boards, setBoards] = useState([])
 
 
     const addNewBoard = () => {
+        console.log(boards)
         setBoards(boards.concat(
             {
+                id: 0,
                 title: "New Section",
                 tasks: []
             }
@@ -28,9 +29,11 @@ function Home() {
                 <Row className={styles.main_row}>
                     {boards
                         ? boards.map((board, index) => {
-                            return <Col sm="12" md="6" lg="3">
-                                <Board key={index} title={board.title} tasks={board.tasks} />
-                            </Col>
+                            return (
+                                <Col sm="12" md="6" lg="3" key={index}>
+                                    <Board key={index} title={board.title} tasks={board.tasks} />
+                                </Col>)
+
                         })
                         : "Loading..."
                     }
@@ -39,7 +42,9 @@ function Home() {
                             <Button onClick={() => addNewBoard()}>ADD</Button>
                         </div>
                     </Col>
+
                 </Row>
+
             </Container>
         </Container>
 
